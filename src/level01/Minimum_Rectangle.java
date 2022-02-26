cb75ab0614ea08cd0bf36a812b3380eadbf8d886
Lsapee <dlworhks94@gmail.com>
2022-02-26 23:27:04 +0900

    Minimum_Rectangle 푸는중...

---
+++ src/level01/Minimum_Rectangle.java
@@ -0,0 +1,14 @@
+package level01;
+
+public class Minimum_Rectangle {
+    public static void main(String[] args) {
+        int [][] sizes ={{60,50},{30,70},{60,30},{80,40}};
+
+        int answer = 0;
+
+        int max_Width=0;
+        int max_hight=0;
+        int num =0;
+
+    }
+}
