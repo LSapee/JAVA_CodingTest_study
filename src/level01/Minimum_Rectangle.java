f349f87ed96bc77d8f403ab068f5e1f9e16fca31
Lsapee <dlworhks94@gmail.com>
2022-03-01 22:02:09 +0900

    백준 브론즈 부터 천천히 푸는중... 프로그래머스 레벨 2

--- src/level01/Minimum_Rectangle.java
+++ src/level01/Minimum_Rectangle.java
@@ -1,14 +1,32 @@
 package level01;
 
+import java.util.Arrays;
+
 public class Minimum_Rectangle {
+
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int max_w =0;
+        int max_h=0;
+        int w=0;
+        int h =0;
+        for(int i =0; i<sizes.length; i++){
+            w=Math.max(sizes[i][0],sizes[i][1]);
+            h=Math.min(sizes[i][0],sizes[i][1]);
+            max_w = Math.max(max_w,w);
+            max_h = Math.max(max_h,h);
+        }
+        answer = max_w*max_h;
+
+        return answer;
+    }
     public static void main(String[] args) {
         int [][] sizes ={{60,50},{30,70},{60,30},{80,40}};
 
         int answer = 0;
+        Minimum_Rectangle mr = new Minimum_Rectangle();
 
-        int max_Width=0;
-        int max_hight=0;
-        int num =0;
+        mr.solution(sizes);
 
     }
 }
