bd0875f8d3f9aa170a830406c1336a793899740c
Lsapee <dlworhks94@gmail.com>
2022-03-12 23:10:03 +0900

    bp study

--- src/bronze/Num10250.java
+++ src/bronze/Num10250.java
@@ -1,4 +1,30 @@
 package bronze;
 
+import java.util.Scanner;
+
 public class Num10250 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int a = sc.nextInt();
+        for(int i=0; i<a; i++){
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            int p = sc.nextInt();
+            int f= p%h;
+            int k = p/h;
+            if(p%h!=0){
+                k++;
+            }else if(p%h ==0){
+                f = h;
+            }
+            String l ="";
+            if(k<10){
+                l = f+"0"+k;
+            }else{
+                l = f+""+k;
+            }
+            System.out.println(Integer.parseInt(l));
+        }
+    }
 }
--- src/bronze/Num2775.java
+++ src/bronze/Num2775.java
@@ -1,5 +1,6 @@
 package bronze;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Num2775 {
@@ -8,11 +9,30 @@ public class Num2775 {
 
         int testcase = sc.nextInt();
         int sum =0;
+        int lsum = 0;
         for(int i =0; i<testcase; i++){
-            int f = sc.nextInt();
-            int p = sc.nextInt();
+            int f = sc.nextInt();//층수
+            int p = sc.nextInt();//호수
+            int all[][] = new int[f][p];
+            for(int j =0; j<p; j++){
+                all[0][j] = j+1;
+            }
+            for(int j = 0; j<f; j++){
+                all[j][0] = 1;
+            }
+            for(int j=1; j<f; j++){
+                for(int k=0;k<p;k++){
+                    sum += all[j][k];
+                }
+                System.out.println(sum);
+            }
 
+            System.out.println(Arrays.toString(all[0]));
+            System.out.println(Arrays.toString(all[1]));
+            System.out.println(Arrays.toString(all[2]));
 
+            int answer = all[f-1][p-1];
+            System.out.println(answer);
         }
     }
 }
