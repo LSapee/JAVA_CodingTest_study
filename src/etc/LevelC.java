cefcc96e47e3e4b427f7c93d813c6c45ea9817db
Lsapee <dlworhks94@gmail.com>
2022-05-11 18:58:38 +0900

    al studying

--- src/etc/LevelC.java
+++ src/etc/LevelC.java
@@ -1,29 +1,30 @@
 package etc;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LevelC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       int h = sc.nextInt();
-       int w = sc.nextInt();
-       int x = sc.nextInt();
-        int k =0;
-       String a = "";
-       for(int i =0; i<h; i++){
-           a = a+sc.next();
-       }
-       String a1[] = a.split("");
-       for(int i=0; i<a1.length; i++){
-           int b = x*(k+1);
-           System.out.print(a1[i]);
-           if(i==b-1){
-               System.out.println();
-               k++;
+       int q = sc.nextInt();
+       for(int i=0; i<q; i++){
+           int n = sc.nextInt();
+            int sum =1;
+           for(int j=2; j<n; j++){
+               if(n%j==0){
+                   sum +=j;
+               }
            }
+           System.out.println(sum);
+            if(n==sum){
+                System.out.println("perfect");
+            }else if(n-sum==1){
+                System.out.println("nearly");
+            }else{
+                System.out.println("neither");
+            }
        }
-
 
     }
 }
