67b0884aceb6b88f84dc61ce9e5933c9deac80f3
Lsapee <dlworhks94@gmail.com>
2022-05-13 23:17:06 +0900

    al studying

--- src/etc/LevelC.java
+++ src/etc/LevelC.java
@@ -1,30 +1,34 @@
 package etc;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LevelC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       int q = sc.nextInt();
-       for(int i=0; i<q; i++){
-           int n = sc.nextInt();
-            int sum =1;
-           for(int j=2; j<n; j++){
-               if(n%j==0){
-                   sum +=j;
-               }
-           }
-           System.out.println(sum);
-            if(n==sum){
-                System.out.println("perfect");
-            }else if(n-sum==1){
-                System.out.println("nearly");
-            }else{
-                System.out.println("neither");
+        int n = sc.nextInt();
+        int sum = 0;
+        int asum = 0;
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            String s2 = sc.next();
+            if (s.equals(s2)) {
+                sum = sum + 2;
+            } else if (s.length() == s2.length()) {
+                String a[] = s.split("");
+                String a1[] = s2.split("");
+                for (int j = 0; j < a.length; j++) {
+                    if (!a[j].equals(a1[j])) {
+                        asum = asum + 1;
+                    }
+                }
+                if(asum<=1){
+                    sum=sum+1;
+                }
+                asum=0;
             }
-       }
-
+        }
+        System.out.println(sum);
     }
-}
+    }
