2fe0ed6885ed161f566cd3cf51a4d7e540125b09
Lsapee <dlworhks94@gmail.com>
2022-05-14 22:50:05 +0900

    al studying

--- src/etc/LevelC.java
+++ src/etc/LevelC.java
@@ -8,27 +8,23 @@ public class LevelC {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int sum = 0;
-        int asum = 0;
-        for (int i = 0; i < n; i++) {
-            String s = sc.next();
-            String s2 = sc.next();
-            if (s.equals(s2)) {
-                sum = sum + 2;
-            } else if (s.length() == s2.length()) {
-                String a[] = s.split("");
-                String a1[] = s2.split("");
-                for (int j = 0; j < a.length; j++) {
-                    if (!a[j].equals(a1[j])) {
-                        asum = asum + 1;
-                    }
-                }
-                if(asum<=1){
-                    sum=sum+1;
-                }
-                asum=0;
+        int m = sc.nextInt();
+        int k[] = new int[n];
+        int a = 0;
+        for(int i=0; i<n; i++){
+            k[i] = sc.nextInt();
+        }
+
+        for(int i =0; i<n; i++){
+            if(k[i]==m){
+                a=i+1;
+                break;
             }
         }
-        System.out.println(sum);
+        if(a==0){
+            System.out.println("unlucky");
+        }else if(a!=0){
+            System.out.println(a);
+        }
     }
-    }
+}
