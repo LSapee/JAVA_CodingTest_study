ed970d0dac1316ee48cf3efb38d92a7b5397aaf7
Lsapee <dlworhks94@gmail.com>
2022-05-17 23:23:25 +0900

    al studying

--- src/etc/LevelC.java
+++ src/etc/LevelC.java
@@ -7,31 +7,22 @@ public class LevelC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int sum =0;
-        int b =0;
-        for(int i =0; i<n; i++){
-            int k = sc.nextInt();
-            b= k;
-            if(k>100000){
-                if(b>750000){
-                    sum+= 65000;
-                }else if(b<=750000){
-                    sum += (b-100000)/10;
-                }
-            }
-            if(k>750000){
-                if(b>1500000){
-                    sum+= 150000;
-                }else if(b<=1500000){
-                    sum += (b-750000)/5;
-                }
-            }
-            if(k>1500000){
-              sum += (k-1500000)/2.5;
-            }
-        }
-        System.out.println(sum);
+       int n = sc.nextInt();
+       int m = sc.nextInt();
 
+       for(int i=0; i<m; i++){
+           int k = sc.nextInt();
+           int p = k/n;
+           int q = p+1;
+           if(k<n){
+               System.out.println(n);
+           }else if(k-p*n <q*n-k){
+               System.out.println(p*n);
+           }else if(k-p*n == q*n-k){
+               System.out.println(q*n);
+           }else if(k-p*n >q*n-k){
+               System.out.println(q*n);
+           }
+       }
     }
 }
