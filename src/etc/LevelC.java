abc0280ddabd8f3da803703b1c5a370836129f59
Lsapee <dlworhks94@gmail.com>
2022-05-26 21:24:08 +0900

    al studying

--- src/etc/LevelC.java
+++ src/etc/LevelC.java
@@ -8,33 +8,8 @@ public class LevelC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int S = sc.nextInt();
-        int P = sc.nextInt();
-
-        int carrot[] = new int[N];
-        int big =0;
-        for(int i=0; i<N; ++i){
-            int mass = sc.nextInt();
-            int suger = sc.nextInt();
-            if(suger>=S-P&&S+P>=suger){
-                carrot[i] = mass;
-            }
-        }
-        for(int i =0; i<N; ++i){
-            big = Math.max(big,carrot[i]);
-        }
-        for(int i=0; i<N; ++i){
-            if(carrot[i]==big&& big!=0){
-                big = i+1;
-            }
-        }
-        if(big>0){
-            System.out.println(big);
-        }else{
-            System.out.println("not found");
-        }
-
+       int a = sc.nextInt();
+        System.out.println(a*12);
 
     }
 }
