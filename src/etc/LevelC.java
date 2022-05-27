61d946dd114f6f4631e10c5d7badf96bc63ca98c
Lsapee <dlworhks94@gmail.com>
2022-05-27 22:03:08 +0900

    al studying

--- src/etc/LevelC.java
+++ src/etc/LevelC.java
@@ -8,8 +8,24 @@ public class LevelC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       int a = sc.nextInt();
-        System.out.println(a*12);
+       String t = sc.nextLine();
+
+       t= t.replaceAll("/"," ");
+       t= t.replaceAll(":", " ");
+        String a[] =t.split(" ");
+
+        int day= Integer.parseInt(a[1]);
+        int m= Integer.parseInt(a[2]);
+        if(m>=24){
+            int k = m/24;
+            m = m%24;
+            day = day+k;
+        }
+        if(m<10){
+            System.out.println(a[0]+"/"+day+" 0"+m+":"+a[3]);
+        }else{
+            System.out.println(a[0]+"/"+day+" "+m+":"+a[3]);
+        }
 
     }
 }
