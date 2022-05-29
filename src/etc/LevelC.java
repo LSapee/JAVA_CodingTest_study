11263e936eec3dbd253ae091aa25bd166c077562
Lsapee <dlworhks94@gmail.com>
2022-05-29 22:25:07 +0900

    al studying

--- src/etc/LevelC.java
+++ src/etc/LevelC.java
@@ -1,31 +1,27 @@
 package etc;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class LevelC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       String t = sc.nextLine();
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+        int p = sc.nextInt();
+        String a ="";
 
-       t= t.replaceAll("/"," ");
-       t= t.replaceAll(":", " ");
-        String a[] =t.split(" ");
-
-        int day= Integer.parseInt(a[1]);
-        int m= Integer.parseInt(a[2]);
-        if(m>=24){
-            int k = m/24;
-            m = m%24;
-            day = day+k;
+        for(int i =s*(p-1) +1;i<=n; ++i){
+            a = a + i+" ";
+            if(i>=s){
+                break;
+            }
         }
-        if(m<10){
-            System.out.println(a[0]+"/"+day+" 0"+m+":"+a[3]);
+        a= a.trim();
+        if(a.length()==0){
+            System.out.println("none");
         }else{
-            System.out.println(a[0]+"/"+day+" "+m+":"+a[3]);
+            System.out.println(a);
         }
-
     }
 }
