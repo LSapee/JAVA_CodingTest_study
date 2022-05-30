fbf54c7967de3639f7dcfa9644ef70524d2140f5
Lsapee <dlworhks94@gmail.com>
2022-05-30 23:27:01 +0900

    al studying 2차원 배열 정렬

--- src/etc/LevelC.java
+++ src/etc/LevelC.java
@@ -1,5 +1,7 @@
 package etc;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class LevelC {
@@ -7,21 +9,36 @@ public class LevelC {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int s = sc.nextInt();
-        int p = sc.nextInt();
-        String a ="";
-
-        for(int i =s*(p-1) +1;i<=n; ++i){
-            a = a + i+" ";
-            if(i>=s){
-                break;
-            }
+        int rank[][] = new int[n][3];
+        String answer = "";
+        for(int i =0; i<n; ++i){
+            rank[i][0] = sc.nextInt();
+            rank[i][1] = sc.nextInt();
+            rank[i][2] = sc.nextInt();
         }
-        a= a.trim();
-        if(a.length()==0){
-            System.out.println("none");
-        }else{
-            System.out.println(a);
+
+       Arrays.sort(rank, new Comparator<int[]>() {
+           @Override
+           public int compare(int[] o1, int[] o2) {
+               if(o1[0] == o2[0]){
+                   if(o1[1] ==o2[1]){
+                       return o2[2]-o1[2];
+                   }
+                   return o2[1]-o1[1];
+               }else{
+                   return o2[0]-o1[0];
+               }
+
+           }
+       });
+
+        for(int i =0; i<rank.length; ++i){
+            for(int j =0; j<3; ++j){
+                answer = answer + " "+rank[i][j];
+            }
+            answer = answer.trim();
+            System.out.println(answer);
+            answer = "";
         }
     }
 }
