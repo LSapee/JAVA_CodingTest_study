c7afb0a7ce9344c93017d45500e1db8b1b72ef78
Lsapee <dlworhks94@gmail.com>
2022-05-31 23:56:06 +0900

    al studying

--- src/etc/LevelC.java
+++ src/etc/LevelC.java
@@ -8,37 +8,29 @@ public class LevelC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int rank[][] = new int[n][3];
-        String answer = "";
-        for(int i =0; i<n; ++i){
-            rank[i][0] = sc.nextInt();
-            rank[i][1] = sc.nextInt();
-            rank[i][2] = sc.nextInt();
+        int m = sc.nextInt();
+        int n = sc.nextInt()-1;
+        int x = sc.nextInt();
+        int answer =x;
+        int aa[] = new int[m];
+        for(int i =0; i<m; ++i){
+            aa[i] = sc.nextInt();
         }
 
-       Arrays.sort(rank, new Comparator<int[]>() {
-           @Override
-           public int compare(int[] o1, int[] o2) {
-               if(o1[0] == o2[0]){
-                   if(o1[1] ==o2[1]){
-                       return o2[2]-o1[2];
-                   }
-                   return o2[1]-o1[1];
-               }else{
-                   return o2[0]-o1[0];
-               }
+        for(int i=0; i<m; ++i){
+            answer = answer-aa[i];
 
-           }
-       });
-
-        for(int i =0; i<rank.length; ++i){
-            for(int j =0; j<3; ++j){
-                answer = answer + " "+rank[i][j];
+            if(answer<=0 && n>0){
+                answer = x;
+                --n;
+                --i;
+                continue;
             }
-            answer = answer.trim();
-            System.out.println(answer);
-            answer = "";
+            if(answer<=0 && n==0){
+                answer = i;
+                break;
+            }
         }
+        System.out.println(answer);
     }
 }
