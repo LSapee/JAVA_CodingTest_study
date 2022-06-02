ff60db31559d7e5ef0fe1c00e5f60817268da2e4
Lsapee <dlworhks94@gmail.com>
2022-06-02 23:58:12 +0900

    al studying

--- src/etc/LevelC.java
+++ src/etc/LevelC.java
@@ -1,34 +1,41 @@
 package etc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class LevelC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int m = sc.nextInt();
-        int n = sc.nextInt()-1;
-        int x = sc.nextInt();
-        int answer =x;
-        int aa[] = new int[m];
-        for(int i =0; i<m; ++i){
-            aa[i] = sc.nextInt();
+        int n = sc.nextInt();
+        int k =0;
+        int f[] = new int[n];
+        int answer = 0;
+        int num =0;
+        for(int i=0; i<n; ++i){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            k = b+a;
+            f[i] =k;
         }
+        ArrayList<Integer> al = new ArrayList<>();
+        Arrays.sort(f);
+        for(int i=0; i<f.length; ++i){
+            al.add(f[i]);
+        }
+        int count[] = new int[n];
 
-        for(int i=0; i<m; ++i){
-            answer = answer-aa[i];
-
-            if(answer<=0 && n>0){
-                answer = x;
-                --n;
-                --i;
-                continue;
-            }
-            if(answer<=0 && n==0){
-                answer = i;
-                break;
+        for(int i =0; i<f.length; ++i){
+            count[i] = Collections.frequency(al,f[i]);
+            System.out.println(count[i]);
+        }
+        num = count[0];
+        for(int i=1; i< count.length; ++i){
+            if(num<count[i]){
+                num =count[i];
+                answer = i+1;
             }
         }
         System.out.println(answer);
