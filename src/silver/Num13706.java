47fe0857d61129e4d2f2ae10da8bc60c04adeb1e
Lsapee <dlworhks94@gmail.com>
2022-07-05 23:23:36 +0900

    Num13706.java

--- src/silver/Num13706.java
+++ src/silver/Num13706.java
@@ -1,22 +1,32 @@
 package silver;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Num13706 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       int n = sc.nextInt();
-       int b = n/2;
-       while(n>1){
-            if(b*b==n){
-                break;
-            }else if(b*b>n){
-                b= b/2;
-            }else if(b*b<n){
-                b= b+b/2;
-            }
+        String n = sc.next();
+        BigInteger num = new BigInteger("2");
+        BigInteger a = new BigInteger("1");
+
+        BigInteger bn = new BigInteger(n);
+        BigInteger max =bn;
+        BigInteger k = new BigInteger("1");
+        BigInteger mid;
+        int i=10;
+        while(true){
+           mid = k.add(max);
+           mid = mid.divide(num);
+           int result = (mid.multiply(mid)).compareTo(bn);
+           if(result==0) break;
+           else if(result ==1){
+               max = mid.subtract(a);
+           }else{
+               k=mid.add(a);
+           }
         }
-        System.out.println(b);
+        System.out.println(mid);
     }
 }
