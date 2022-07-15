d84da68529892aaebfab26be927d460ac3518e8c
Lsapee <dlworhks94@gmail.com>
2022-07-15 23:22:03 +0900

    아무거나 빨리 풀고 쉬기

---
+++ src/etc/D214.java
@@ -0,0 +1,13 @@
+package etc;
+
+import java.util.Scanner;
+
+public class D214 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a-b);
+    }
+}
