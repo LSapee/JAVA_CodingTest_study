66ab6ad7b2916d9ecd560f4588d1b064720cffa5
Lsapee <dlworhks94@gmail.com>
2022-07-16 22:07:19 +0900

    다른공부중

---
+++ src/etc/D215.java
@@ -0,0 +1,17 @@
+package etc;
+
+import java.util.Scanner;
+
+public class D215 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        if(s.equals("paiza")){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
