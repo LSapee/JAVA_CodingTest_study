a550c0cf54ecbf111eca9dbd1e5aa466219e4468
Lsapee <dlworhks94@gmail.com>
2022-07-20 23:28:08 +0900

    다른공부중 열개씩 끊어 출력

---
+++ src/bronze/Num11721.java
@@ -0,0 +1,20 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Num11721 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String s1[] = s.split("");
+        StringBuilder ss = new StringBuilder();
+        for(int i=0; i<s1.length; ++i){
+            if(i%10==0){
+                ss.append("\n");
+            }
+            ss.append(s1[i]);
+        }
+        System.out.println(ss);
+    }
+}
