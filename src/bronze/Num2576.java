8569061865169ff66db64a53c1e36760a933e0ff
Lsapee <dlworhks94@gmail.com>
2022-07-22 22:42:47 +0900

    홀수

---
+++ src/bronze/Num2576.java
@@ -0,0 +1,27 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Num2576 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int min = Integer.MAX_VALUE;
+        int sum =0;
+        for(int i=0; i<7; ++i){
+            int a = sc.nextInt();
+            if(a%2!=0){
+                sum+=a;
+                min = Math.min(min,a);
+            }
+        }
+        if(sum ==0){
+            System.out.println(-1);
+
+        }else{
+            System.out.println(sum);
+            System.out.println(min);
+        }
+
+    }
+}
