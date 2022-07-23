9f10b7baa2fe3c5e70bad5b46d2f888c39f521fa
Lsapee <dlworhks94@gmail.com>
2022-07-23 22:57:49 +0900

    not thing

---
+++ src/bronze/Num2953.java
@@ -0,0 +1,30 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Num2953 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum =0;
+        int cookscore[] = new int[5];
+        int count = 0;
+        int maxscore = Integer.MIN_VALUE;
+        for(int i=1; i<=20; ++i){
+            int a = sc.nextInt();
+            sum += a;
+            if(i%4==0){
+                cookscore[count] = sum;
+                sum=0;
+                ++count;
+            }
+        }
+        for(int i=0; i<5; ++i){
+            maxscore = Math.max(maxscore,cookscore[i]);
+            if(maxscore==cookscore[i]){
+                count=i+1;
+            }
+        }
+        System.out.println(count+" "+maxscore);
+    }
+}
