833cf69216789980a0ea5d254ab03448f91fd6d4
Lsapee <dlworhks94@gmail.com>
2022-08-10 22:29:12 +0900

    별찍기 OK

---
+++ src/bronze/Num2523.java
@@ -0,0 +1,26 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Num2523 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1;i<=n; ++i){
+            for(int j=0; j<i;++j){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for(int i=1;i<n; ++i){
+            for(int j=0; j<n-i;++j){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
