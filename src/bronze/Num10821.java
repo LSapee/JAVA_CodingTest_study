19a5822abed23d862e8c3e2f15a07c8366ed2585
Lsapee <dlworhks94@gmail.com>
2022-08-30 18:29:27 +0900

    ...

---
+++ src/bronze/Num10821.java
@@ -0,0 +1,14 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Num10821 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String []ss = s.split(",");
+        System.out.println(ss.length);
+
+    }
+}
