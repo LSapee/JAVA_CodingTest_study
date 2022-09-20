a726553802a6fcce78bdb7aef05de285c0a027ba
leejk <dlworhks94@gmail.com>
2022-09-20 22:23:30 +0900

    재귀

---
+++ src/bronze/Num25501.java
@@ -0,0 +1,35 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Num25501 {
+
+    static int count =1;
+    public static int recursion(String s, int l, int r){
+        if(l >= r) {
+            return 1;
+        }
+        else if(s.charAt(l) != s.charAt(r)){
+            return 0;
+        }
+        else{
+            count++;
+            return recursion(s, l+1, r-1);
+        }
+
+    }
+    public static int isPalindrome(String s){
+        count=1;
+        return recursion(s, 0, s.length()-1);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i =0; i<n; ++i){
+            String s =sc.next();
+            System.out.println(isPalindrome(s)+" "+count);
+        }
+
+    }
+}
