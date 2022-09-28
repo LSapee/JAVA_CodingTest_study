2746489e9dfbc7f6f407762bce97fd23ab059da3
leejk <dlworhks94@gmail.com>
2022-09-28 16:13:29 +0900

    Scanner 와 BufferedReader 의 속도 차이

--- src/bronze/Num10989.java
+++ src/bronze/Num10989.java
@@ -1,20 +1,25 @@
 package bronze;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Num10989 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(bf.readLine());
+        StringBuilder sb =new StringBuilder();
         int[] a = new int[n];
         for(int i=0; i<n; ++i){
-            a[i] = sc.nextInt();
+            a[i] = Integer.parseInt(bf.readLine());
         }
         Arrays.sort(a);
         for(int i=0; i<n; ++i){
-            System.out.println(a[i]);
+            sb.append(a[i]).append("\n");
         }
+        System.out.println(sb);
     }
 }
