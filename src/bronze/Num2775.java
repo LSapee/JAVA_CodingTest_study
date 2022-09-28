414f5c7055c4dfe479ebe88fce946bfa827d4965
leejk <dlworhks94@gmail.com>
2022-09-28 16:04:27 +0900

    수학

--- src/bronze/Num2775.java
+++ src/bronze/Num2775.java
@@ -9,30 +9,25 @@ public class Num2775 {
 
         int testcase = sc.nextInt();
         int sum =0;
-        int lsum = 0;
-        for(int i =0; i<testcase; i++){
-            int f = sc.nextInt();//층수
-            int p = sc.nextInt();//호수
-            int all[][] = new int[f][p];
-            for(int j =0; j<p; j++){
-                all[0][j] = j+1;
-            }
-            for(int j = 0; j<f; j++){
-                all[j][0] = 1;
-            }
-            for(int j=1; j<f; j++){
-                for(int k=0;k<p;k++){
-                    sum += all[j][k];
+
+        for(int i=0; i<testcase; ++i){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int[][] ho = new int[a+1][b];
+            for(int j=0; j<=a;++j){
+                for(int k=0; k<b; ++k){
+                    if(j==0){
+                        ho[j][k] = k+1;
+                    }else{
+                        for(int n =0; n<=k; ++n){
+                            sum += ho[j-1][n];
+                        }
+                        ho[j][k] = sum;
+                        sum =0;
+                    }
                 }
-                System.out.println(sum);
             }
-
-            System.out.println(Arrays.toString(all[0]));
-            System.out.println(Arrays.toString(all[1]));
-            System.out.println(Arrays.toString(all[2]));
-
-            int answer = all[f-1][p-1];
-            System.out.println(answer);
+            System.out.println(ho[a][b-1]);
         }
     }
 }
