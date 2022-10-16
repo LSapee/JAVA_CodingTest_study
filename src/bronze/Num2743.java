9427b626b8bbceff15aa1d6812ffaa6ec14680e1
leejk <dlworhks94@gmail.com>
2022-10-16 22:48:07 +0900

    브론즈 1문제

---
+++ src/bronze/Num2743.java
@@ -0,0 +1,12 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(bf.readLine().length());
+    }
+}
