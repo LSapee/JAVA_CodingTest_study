29673c218390f7fc38cc371afe00e26916f2dcce
leejk <dlworhks94@gmail.com>
2022-10-19 23:25:49 +0900

    이분탐색 x hashmap으로 품 hashmap에 int값을key 벨류는 1로 넣고 중복값을 넣어야할경우 key를 이용하여 벨류값을 빼고 다시 벨류에+1하여 다시 map에 넣음 이후 원하는 숫자 카드를 찾을 때 key값으로 벨류 값을 가져옴 지정되지 않은 key값을 경우 null로 결과값이 나오기에 null의 경우 0으로 치환하여 sb에 추가함

--- src/silver/Num10816.java
+++ src/silver/Num10816.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Num10816 {
@@ -12,66 +13,27 @@ public class Num10816 {
         StringTokenizer st = new StringTokenizer(bf.readLine());
         StringBuilder sb =new StringBuilder();
         int n = Integer.parseInt(st.nextToken());
-        int arr[] = new int[n];
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
         st = new StringTokenizer(bf.readLine());
         for(int i=0; i<n; ++i){
             int a = Integer.parseInt(st.nextToken());
-            arr[i] = a;
+            if(hashMap.containsKey(a)){
+                hashMap.put(a,hashMap.get(a)+1);
+            }else{
+                hashMap.put(a,1);
+            }
         }
-        Arrays.sort(arr);
         int m =Integer.parseInt(bf.readLine());
         st = new StringTokenizer(bf.readLine());
-
         for(int i=0; i<m; ++i){
-            int a= Integer.parseInt(st.nextToken());
-            int c = binarySearch(a,arr);
-            int d = binarySearch2(a,arr);
-            sb.append(c+d).append(" ");
+            int a = Integer.parseInt(st.nextToken());
+            if(hashMap.get(a)==null){
+                sb.append(0).append(" ");
+            }else{
+                sb.append(hashMap.get(a)).append(" ");
+            }
         }
         System.out.println(sb.toString().trim());
     }
 
-    static int binarySearch(int a,int[] arr){
-        int mid =0;
-        int left =0;
-        int count =0;
-        int right = arr.length-1;
-        while(left<=right){
-            mid = (left+right)/2;
-            if(arr[mid]==a){
-                while(mid!=0&&arr[mid-1]==a){
-                    --mid;
-                    count++;
-                }
-                return count;
-            }else if(arr[mid]>a){
-                right = mid-1;
-            }else if(arr[mid]<a){
-                left = mid+1;
-            }
-        }
-        return count;
-    }
-    static int binarySearch2(int a,int[] arr){
-        int mid =0;
-        int left =0;
-        int count =0;
-        int right = arr.length-1;
-        while(left<=right){
-            mid = (left+right)/2;
-            if(arr[mid]==a){
-                count =1;
-                while(mid<arr.length-1&&arr[mid+1]==a){
-                    count++;
-                    mid++;
-                }
-                return count;
-            }else if(arr[mid]>a){
-                right = mid-1;
-            }else if(arr[mid]<a){
-                left = mid+1;
-            }
-        }
-        return count;
-    }
 }
