096f482f34f40ad0081d3bb726e764bad88de1d9
leejk <dlworhks94@gmail.com>
2022-10-27 00:01:14 +0900

    이전에 실패했었던 문제들 수정 + 브론즈 몇문제

---
+++ src/bronze/Num24082.java
@@ -0,0 +1,11 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Num24082 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n*n*n);
+    }
+}
---
+++ src/bronze/Num24086.java
@@ -0,0 +1,12 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Num24086 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int m =sc.nextInt();
+        System.out.println(m-n);
+    }
+}
